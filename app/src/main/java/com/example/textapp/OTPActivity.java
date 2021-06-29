ab57ccdd814534133ec2e256c509264cb2e6f7f9
Electromorphous <pstation.474@gmail.com>
2021-06-29 23:52:44 +0530

    Implement user handling with firebase

--- app/src/main/java/com/example/textapp/OTPActivity.java
+++ app/src/main/java/com/example/textapp/OTPActivity.java
@@ -3,21 +3,19 @@ package com.example.textapp;
 import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.app.ProgressDialog;
 import android.content.Context;
+import android.content.Intent;
 import android.os.Bundle;
 import android.view.inputmethod.InputMethodManager;
 import android.widget.Toast;
 
 import com.example.textapp.databinding.ActivityOtpactivityBinding;
-import com.google.android.gms.tasks.OnCompleteListener;
-import com.google.android.gms.tasks.Task;
 import com.google.firebase.FirebaseException;
-import com.google.firebase.auth.AuthResult;
 import com.google.firebase.auth.FirebaseAuth;
 import com.google.firebase.auth.PhoneAuthCredential;
 import com.google.firebase.auth.PhoneAuthOptions;
 import com.google.firebase.auth.PhoneAuthProvider;
-import com.mukesh.OnOtpCompletionListener;
 
 import java.util.concurrent.TimeUnit;
 
@@ -25,6 +23,7 @@ public class OTPActivity extends AppCompatActivity {
 
     ActivityOtpactivityBinding binding;
     FirebaseAuth auth;
+    ProgressDialog dialog;
 
     String verificationId;
 
@@ -34,10 +33,15 @@ public class OTPActivity extends AppCompatActivity {
         binding = ActivityOtpactivityBinding.inflate(getLayoutInflater());
         setContentView(binding.getRoot());
 
+        dialog = new ProgressDialog(this);
+        dialog.setMessage("Sending OTP...");
+        dialog.setCancelable(false);
+        dialog.show();
+
         auth = FirebaseAuth.getInstance();
 
         String phoneNumber = getIntent().getStringExtra("phoneNumber");
-        binding.phoneLabel.setText("Verify " + phoneNumber);
+        binding.verifyLabel.setText("Verify " + phoneNumber);
 
         PhoneAuthOptions options = PhoneAuthOptions.newBuilder(auth)
                 .setPhoneNumber(phoneNumber)
@@ -58,6 +62,8 @@ public class OTPActivity extends AppCompatActivity {
                     public void onCodeSent(@NonNull String s, @NonNull PhoneAuthProvider.ForceResendingToken forceResendingToken) {
                         super.onCodeSent(s, forceResendingToken);
 
+                        dialog.dismiss();
+
                         verificationId = s;
 
                         InputMethodManager imm = (InputMethodManager) getSystemService(Context.INPUT_METHOD_SERVICE);
@@ -69,24 +75,18 @@ public class OTPActivity extends AppCompatActivity {
 
         PhoneAuthProvider.verifyPhoneNumber(options);
 
-        binding.otpView.setOtpCompletionListener(new OnOtpCompletionListener() {
-            @Override
-            public void onOtpCompleted(String otp) {
-                PhoneAuthCredential credential = PhoneAuthProvider.getCredential(verificationId, otp);
+        binding.otpView.setOtpCompletionListener(otp -> {
+            PhoneAuthCredential credential = PhoneAuthProvider.getCredential(verificationId, otp);
 
-                auth.signInWithCredential(credential).addOnCompleteListener(new OnCompleteListener<AuthResult>() {
-                    @Override
-                    public void onComplete(@NonNull Task<AuthResult> task) {
-                        if(task.isSuccessful()) {
-                            Toast.makeText(OTPActivity.this, "Logged in", Toast.LENGTH_SHORT).show();
-                        }
-                        else {
-                            Toast.makeText(OTPActivity.this, "Login failed", Toast.LENGTH_SHORT).show();
-                        }
-                    }
-                });
-            }
+            auth.signInWithCredential(credential).addOnCompleteListener(task -> {
+                if (task.isSuccessful()) {
+                    Intent intent = new Intent(OTPActivity.this, SetupProfileActivity.class);
+                    startActivity(intent);
+                    finishAffinity();
+                } else {
+                    Toast.makeText(OTPActivity.this, "Login failed", Toast.LENGTH_SHORT).show();
+                }
+            });
         });
-
     }
 }
---
+++ app/src/main/java/com/example/textapp/SetupProfileActivity.java
@@ -0,0 +1,120 @@
+package com.example.textapp;
+
+import androidx.annotation.Nullable;
+import androidx.annotation.RequiresApi;
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.app.ProgressDialog;
+import android.content.Intent;
+import android.net.Uri;
+import android.os.Build;
+import android.os.Bundle;
+
+import com.example.textapp.databinding.ActivitySetupProfileBinding;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.storage.FirebaseStorage;
+import com.google.firebase.storage.StorageReference;
+
+import java.util.Objects;
+
+public class SetupProfileActivity extends AppCompatActivity {
+
+    ActivitySetupProfileBinding binding;
+    FirebaseAuth auth;
+    FirebaseDatabase database;
+    FirebaseStorage storage;
+    Uri selectedImage;
+    ProgressDialog dialog;
+
+    @RequiresApi(api = Build.VERSION_CODES.M)
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        binding = ActivitySetupProfileBinding.inflate(getLayoutInflater());
+        setContentView(binding.getRoot());
+
+        dialog = new ProgressDialog(this);
+        dialog.setMessage("Updating profile...");
+        dialog.setCancelable(false);
+
+        auth = FirebaseAuth.getInstance();
+        database = FirebaseDatabase.getInstance();
+        storage = FirebaseStorage.getInstance();
+
+        binding.imageView.setOnClickListener(v -> {
+            Intent intent = new Intent();
+            intent.setAction(Intent.ACTION_GET_CONTENT);
+            intent.setType("image/*");
+            startActivityForResult(intent, 45);
+        });
+
+        binding.setupButton.setOnClickListener(v -> {
+            String name = binding.nameBox.getText().toString();
+
+            if (name.isEmpty()) {
+                binding.nameBox.setError("Please enter a name");
+                return;
+            }
+
+            dialog.show();
+
+            if (selectedImage != null) {
+                StorageReference ref = storage.getReference().child("Profiles").child(Objects.requireNonNull(auth.getUid()));
+                ref.putFile(selectedImage).addOnCompleteListener(task -> {
+                    if (task.isSuccessful()) {
+                        ref.getDownloadUrl().addOnSuccessListener(uri -> {
+                            String uid = auth.getUid();
+                            String name1 = binding.nameBox.getText().toString();
+                            String phoneNumber = Objects.requireNonNull(auth.getCurrentUser()).getPhoneNumber();
+                            String imageUrl = uri.toString();
+
+                            User user = new User(uid, name1, phoneNumber, imageUrl);
+
+                            database.getReference()
+                                    .child("users")
+                                    .child(uid)
+                                    .setValue(user)
+                                    .addOnSuccessListener(unused -> {
+                                        dialog.dismiss();
+
+                                        Intent intent = new Intent(SetupProfileActivity.this, MainActivity.class);
+                                        startActivity(intent);
+                                        finish();
+                                    });
+                        });
+                    }
+                });
+            } else {
+                String uid = auth.getUid();
+                String phoneNumber = Objects.requireNonNull(auth.getCurrentUser()).getPhoneNumber();
+
+                User user = new User(uid, name, phoneNumber, "No image");
+
+                assert uid != null;
+                database.getReference()
+                        .child("users")
+                        .child(uid)
+                        .setValue(user)
+                        .addOnSuccessListener(unused -> {
+                            dialog.dismiss();
+                            Intent intent = new Intent(SetupProfileActivity.this, MainActivity.class);
+                            startActivity(intent);
+                            finish();
+                        });
+            }
+        });
+    }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, @Nullable Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+
+        if (data != null) {
+            if (data.getData() != null) {
+                binding.imageView.setImageURI(data.getData());
+                selectedImage = data.getData();
+            }
+        }
+    }
+}
