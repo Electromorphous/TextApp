89fb9c9dd5397ee106c0894040892cdaa0d67846
Electromorphous <pstation.474@gmail.com>
2021-06-30 16:33:31 +0530

    design main activity

--- app/src/main/java/com/example/textapp/PhoneNumberActivity.java
+++ app/src/main/java/com/example/textapp/PhoneNumberActivity.java
@@ -6,16 +6,25 @@ import android.content.Intent;
 import android.os.Bundle;
 
 import com.example.textapp.databinding.ActivityPhoneNumberBinding;
+import com.google.firebase.auth.FirebaseAuth;
 
 public class PhoneNumberActivity extends AppCompatActivity {
 
     ActivityPhoneNumberBinding binding;
+    FirebaseAuth auth;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         binding = ActivityPhoneNumberBinding.inflate(getLayoutInflater());
         setContentView(binding.getRoot());
+
+        auth = FirebaseAuth.getInstance();
+        if (auth.getCurrentUser() != null) {
+            Intent intent = new Intent(PhoneNumberActivity.this, MainActivity.class);
+            startActivity(intent);
+            finish();
+        }
 
         binding.continueButton.setOnClickListener(v -> {
             Intent intent = new Intent(PhoneNumberActivity.this, OTPActivity.class);
