108e74b50915208dc4081a6f896e49d44eaeac74
Electromorphous <pstation.474@gmail.com>
2021-06-30 18:31:50 +0530

    display users on home

--- app/src/main/java/com/example/textapp/MainActivity.java
+++ app/src/main/java/com/example/textapp/MainActivity.java
@@ -1,16 +1,57 @@
 package com.example.textapp;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
 import android.view.Menu;
 
+import com.example.textapp.databinding.ActivityMainBinding;
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
+import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.ValueEventListener;
+
+import java.util.ArrayList;
+
 public class MainActivity extends AppCompatActivity {
+
+    ActivityMainBinding binding;
+    FirebaseDatabase database;
+    ArrayList<User> users;
+    UsersAdapter usersAdapter;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        binding = ActivityMainBinding.inflate(getLayoutInflater());
+        setContentView(binding.getRoot());
+
+        database = FirebaseDatabase.getInstance();
+        users = new ArrayList<>();
+        usersAdapter = new UsersAdapter(this, users);
+
+        binding.recyclerView.setAdapter(usersAdapter);
+
+        database.getReference().child("users").addValueEventListener(new ValueEventListener() {
+            @Override
+            public void onDataChange(@NonNull DataSnapshot snapshot) {
+                users.clear();
+
+                for(DataSnapshot snapshot1 : snapshot.getChildren()) {
+                    User user = snapshot1.getValue(User.class);
+                    users.add(user);
+                }
+                usersAdapter.notifyDataSetChanged();
+            }
+
+            @Override
+            public void onCancelled(@NonNull DatabaseError error) {
+
+            }
+        });
     }
 
     @Override
---
+++ app/src/main/java/com/example/textapp/UsersAdapter.java
@@ -0,0 +1,57 @@
+package com.example.textapp;
+
+import android.content.Context;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import androidx.annotation.DrawableRes;
+import androidx.annotation.NonNull;
+import androidx.recyclerview.widget.RecyclerView;
+
+import com.bumptech.glide.Glide;
+import com.example.textapp.databinding.RowConversationBinding;
+
+import java.util.ArrayList;
+
+public class UsersAdapter extends RecyclerView.Adapter<UsersAdapter.UsersViewHolder> {
+
+    Context context;
+    ArrayList<User> users;
+
+    public UsersAdapter(Context context, ArrayList<User> users) {
+        this.context = context;
+        this.users = users;
+    }
+
+    @NonNull
+    @Override
+    public UsersViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
+        View view = LayoutInflater.from(context).inflate(R.layout.row_conversation, parent, false);
+        return new UsersViewHolder(view);
+    }
+
+    @Override
+    public void onBindViewHolder(@NonNull UsersAdapter.UsersViewHolder holder, int position) {
+        User user = users.get(position);
+
+        holder.binding.username.setText(user.getName());
+
+        Glide.with(context).load(user.getProfileImage()).placeholder(R.drawable.avatar).into(holder.binding.profile);
+    }
+
+    @Override
+    public int getItemCount() {
+        return users.size();
+    }
+
+    public static class UsersViewHolder extends RecyclerView.ViewHolder {
+
+        RowConversationBinding binding;
+
+        public UsersViewHolder(@NonNull View itemView) {
+            super(itemView);
+            binding = RowConversationBinding.bind(itemView);
+        }
+    }
+}
